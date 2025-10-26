a9f4f38de69de62b84598151a9d00dd183126570
vgungor <mvolkang@gmail.com>
2025-10-27 01:12:02 +0300

    servis takip, design of forms and pages added via cursor

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ app.whenReady().then(() => {
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 900,
-    height: 600,
+    width: 1200,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
